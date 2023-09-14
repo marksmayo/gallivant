c45ecd4a666d95bb18f0755ffb03042a401c72de
Mark Mayo <mark@there.co.nz>
2023-09-14 16:18:14 +1200

    added tree-view and timestamps

--- gallivant.py
+++ gallivant.py
@@ -1,16 +1,19 @@
+from datetime import datetime
 from PyQt5.QtCore import *
 from PyQt5.QtGui import QIcon
 from PyQt5.QtWebChannel import QWebChannel
 from PyQt5.QtWebEngineWidgets import QWebEngineView, QWebEnginePage
-from PyQt5.QtWidgets import QApplication, QMainWindow, QListWidget, QHBoxLayout, QVBoxLayout, QWidget, QAction, QMenu, QMenuBar, QInputDialog, QLineEdit, QDialog, QLabel, QPushButton, QMessageBox
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTreeWidgetItem, QHBoxLayout, QWidget, QAction, QMenu, QMenuBar, QInputDialog, QLineEdit, QMessageBox, QTreeWidget
+
 
 class MyPage(QWebEnginePage):
     def javaScriptConsoleMessage(self, level, msg, line, sourceID):
         print("JS console message:", msg)
 
-class MyWindow(QMainWindow):
+
+class Gallivant(QMainWindow):
     def __init__(self):
-        super(MyWindow, self).__init__()
+        super().__init__()
 
         self.setWindowIcon(QIcon('images/gallivant.png'))
         self.setWindowTitle("Gallivant - an exploratory testing tool")
@@ -21,6 +24,10 @@ class MyWindow(QMainWindow):
         # Create File menu and add it to menu bar
         self.fileMenu = QMenu('Session', self)
         self.menuBar.addMenu(self.fileMenu)
+
+        # Initialize QTreeWidget to keep track of clicked elements and sentences
+        self.treeWidget = QTreeWidget()
+        self.treeWidget.setHeaderLabels(["Annotation", "Details", "Timestamp"])
 
         # Create Exit action and add it to File menu
         self.exitAction = QAction('Exit', self)
@@ -34,22 +41,18 @@ class MyWindow(QMainWindow):
         self.aboutAction.triggered.connect(self.showAbout)
         self.helpMenu.addAction(self.aboutAction)
 
-
         # Set the menu bar
         self.setMenuBar(self.menuBar)
 
         self.browser = QWebEngineView()
         self.browser.setPage(MyPage(self.browser))
         self.browser.setUrl(QUrl("https://www.google.com"))
-        
-        self.browser.loadFinished.connect(self.onLoadFinished)
 
-        # Initialize QListWidget to keep track of clicked elements and sentences
-        self.listWidget = QListWidget()
+        self.browser.loadFinished.connect(self.onLoadFinished)
 
         # Main layout and widget
         layout = QHBoxLayout()
-        layout.addWidget(self.listWidget)
+        layout.addWidget(self.treeWidget)
         layout.addWidget(self.browser)
 
         container = QWidget()
@@ -71,8 +74,6 @@ class MyWindow(QMainWindow):
         dialog.setWindowTitle("About Gallivant")
         dialog.setIcon(QMessageBox.Information)
         dialog.exec()
-
-
 
     @pyqtSlot(bool)
     def onLoadFinished(self, ok):
@@ -103,17 +104,39 @@ class MyWindow(QMainWindow):
     @pyqtSlot(str)
     def elementClicked(self, elementInfo):
         elementInfo = eval(elementInfo)
-        sentence = "This is a sample sentence."
         current_url = self.browser.url().toString()
         text, okPressed = QInputDialog.getText(self, "Annotation", "Your sentence:", QLineEdit.Normal, "")
         if okPressed and text != '':
-            entry = f"URL: {current_url}, XPath: {elementInfo['tag']}, ID: {elementInfo['id']}, Class: {elementInfo['class']}, Text: {elementInfo['text']} - {text}"
-            self.listWidget.addItem(entry)
+            entry = f"{text}"
+            timestamp = datetime.now().strftime("%H:%M:%S")  # Get current time
 
+            item = QTreeWidgetItem(self.treeWidget)
+            item.setText(0, entry)
+            item.setText(2, timestamp)  # Add timestamp to the new colum
+
+            child1 = QTreeWidgetItem(item)
+            child1.setText(0, "URL")
+            child1.setText(1, current_url)
+
+            child2 = QTreeWidgetItem(item)
+            child2.setText(0, "XPath")
+            child2.setText(1, elementInfo['tag'])
+
+            child3 = QTreeWidgetItem(item)
+            child3.setText(0, "ID")
+            child3.setText(1, elementInfo['id'])
+
+            child4 = QTreeWidgetItem(item)
+            child4.setText(0, "Class")
+            child4.setText(1, elementInfo['class'])
+
+            child5 = QTreeWidgetItem(item)
+            child5.setText(0, "Text")
+            child5.setText(1, elementInfo['text'])
 
 
 if __name__ == "__main__":
     app = QApplication([])
-    myWin = MyWindow()
+    myWin = Gallivant()
     myWin.show()
     app.exec_()
