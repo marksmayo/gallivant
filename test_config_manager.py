196c564dc8b98eb9ea11840a185494300853cad9
Mark Mayo <mark.mayo@myob.com>
2023-09-15 23:59:34 +1200

    added pytest and some tests for the config

---
+++ test_config_manager.py
@@ -0,0 +1,27 @@
+# test_config_loader.py
+import pytest
+import json
+from config_manager import load_config
+
+def test_load_valid_config(mocker):
+    mock_file_content = '{"key": "value"}'
+    mocker.patch('builtins.open', mocker.mock_open(read_data=mock_file_content))
+    mocker.patch('json.load', return_value=json.loads(mock_file_content))
+
+    result = load_config()
+
+    assert result == {"key": "value"}
+
+def test_load_corrupted_config(mocker):
+    mock_file_content = '{"key": "value" '  # Corrupted JSON
+    mocker.patch('builtins.open', mocker.mock_open(read_data=mock_file_content))
+    mocker.patch('json.load', side_effect=json.JSONDecodeError("Invalid JSON", "doc", 0))
+
+    with pytest.raises(json.JSONDecodeError):
+        load_config()
+
+def test_load_config_file_not_found(mocker):
+    mocker.patch('builtins.open', side_effect=FileNotFoundError("File not found"))
+
+    with pytest.raises(FileNotFoundError):
+        load_config()
