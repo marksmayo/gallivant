f7ecf52e53fc2ada34b107e2a1e0d5c794c602a1
Mark Mayo <mark.mayo@myob.com>
2023-09-16 00:00:25 +1200

    black formatting adjustment

--- test_config_manager.py
+++ test_config_manager.py
@@ -3,25 +3,30 @@ import pytest
 import json
 from config_manager import load_config
 
+
 def test_load_valid_config(mocker):
     mock_file_content = '{"key": "value"}'
-    mocker.patch('builtins.open', mocker.mock_open(read_data=mock_file_content))
-    mocker.patch('json.load', return_value=json.loads(mock_file_content))
+    mocker.patch("builtins.open", mocker.mock_open(read_data=mock_file_content))
+    mocker.patch("json.load", return_value=json.loads(mock_file_content))
 
     result = load_config()
 
     assert result == {"key": "value"}
 
+
 def test_load_corrupted_config(mocker):
     mock_file_content = '{"key": "value" '  # Corrupted JSON
-    mocker.patch('builtins.open', mocker.mock_open(read_data=mock_file_content))
-    mocker.patch('json.load', side_effect=json.JSONDecodeError("Invalid JSON", "doc", 0))
+    mocker.patch("builtins.open", mocker.mock_open(read_data=mock_file_content))
+    mocker.patch(
+        "json.load", side_effect=json.JSONDecodeError("Invalid JSON", "doc", 0)
+    )
 
     with pytest.raises(json.JSONDecodeError):
         load_config()
 
+
 def test_load_config_file_not_found(mocker):
-    mocker.patch('builtins.open', side_effect=FileNotFoundError("File not found"))
+    mocker.patch("builtins.open", side_effect=FileNotFoundError("File not found"))
 
     with pytest.raises(FileNotFoundError):
         load_config()
