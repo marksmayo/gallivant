ea5e446d818dfa00d82e37e5d4613ed08d500eca
Mark Mayo <mark.mayo@myob.com>
2023-09-16 12:55:35 +1200

    change to initialsie in function

--- test_web_engine.py
+++ test_web_engine.py
@@ -1,19 +1,13 @@
 import sys
 import pytest
 from PyQt5.QtWidgets import QApplication
-from PyQt5.QtCore import QUrl
 from webengine import WebEngine
 
-# Create an instance of QApplication. Required for PyQt5.
-app = QApplication(sys.argv)
-
-
 def test_java_script_console_message(mocker):
-    # Mock the built-in print function
+    app = QApplication(sys.argv)
     mock_print = mocker.patch("builtins.print")
 
     page = WebEngine()
     page.javaScriptConsoleMessage(None, "Test message", 0, "source")
 
-    # Assert that the print function is called with the correct arguments.
     mock_print.assert_called_once_with("JS console message:", "Test message")
