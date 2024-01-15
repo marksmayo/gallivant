ed4dab676cf767947fb6e8c7a8d756ba4b4613b4
Mark Mayo <mark@there.co.nz>
2024-01-15 16:49:44 +1300

    Fix to prevent Ctrl-clicks from actually navigating at the same time

--- widgets.py
+++ widgets.py
@@ -59,6 +59,8 @@ class Browser(QWebEngineView):
 
                     document.addEventListener('click', function(event) {
                         if (event.ctrlKey) {
+                            event.preventDefault();
+                            event.stopPropagation();                        
                             var element = event.target;
                             element.style.border = "2px solid #FF0000";  // Red border
                             var elementInfo = {
